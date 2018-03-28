6cc797ba79e828cb5645d69e69fb5f85dbf22385
Darshana Rathanayake <drox2014@gmail.com>
2018-03-28 20:12:35 +0530

    start script updated

--- src/db/redis.ts
+++ src/db/redis.ts
@@ -1,7 +1,9 @@
 import redis from "redis";
 
 const client = redis.createClient({
-    host: process.env.REDIS_URL || "127.0.0.1"
+    host: process.env.REDIS_URL,
+    port: 21179,
+    password: "pd2c7acb80d6c99a31285b71a698ab86867df7eba0b55d3dd3c3d283663c5df44"
 });
 
 client.on("connect", () => console.log("Connected to Redis"));
