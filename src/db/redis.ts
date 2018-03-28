21289090c71626df71d2ee24131e2097028d630e
Darshana Rathanayake <drox2014@gmail.com>
2018-03-28 20:05:46 +0530

    start script updated

--- src/db/redis.ts
+++ src/db/redis.ts
@@ -1,6 +1,8 @@
 import redis from "redis";
 
-const client = redis.createClient();
+const client = redis.createClient({
+    host: process.env.REDIS_URL || "127.0.0.1"
+});
 
 client.on("connect", () => console.log("Connected to Redis"));
 client.on("error", (e) => console.log(`Unable to connect to Redis ${e.message}`));
