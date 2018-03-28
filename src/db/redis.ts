7d070f9c884b2de0f0fc10c785507c19f25b8a92
Darshana Rathanayake <drox2014@gmail.com>
2018-03-28 20:16:08 +0530

    start script updated

--- src/db/redis.ts
+++ src/db/redis.ts
@@ -1,7 +1,8 @@
 import redis from "redis";
 
 const client = redis.createClient({
-    host: process.env.REDIS_URL,
+    host: "ec2-34-193-123-212.compute-1.amazonaws.com",
+    url: "redis://h:pd2c7acb80d6c99a31285b71a698ab86867df7eba0b55d3dd3c3d283663c5df44@ec2-34-193-123-212.compute-1.amazonaws.com:21179",
     port: 21179,
     password: "pd2c7acb80d6c99a31285b71a698ab86867df7eba0b55d3dd3c3d283663c5df44"
 });
