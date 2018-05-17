7920e7e9f08c030edf49359fcebb21b65c73c711
Darshana Rathanayake <drox2014@gmail.com>
2018-05-17 11:09:29 +0530

    redis vars

--- dist/db/redis.js
+++ dist/db/redis.js
@@ -11,12 +11,24 @@ const redis_1 = __importDefault(require("redis"));
 /**
  * This function will use an URI to internet redis database.
  */
-const client = redis_1.default.createClient({
-    host: "ec2-34-193-123-212.compute-1.amazonaws.com",
-    url: "redis://h:pd2c7acb80d6c99a31285b71a698ab86867df7eba0b55d3dd3c3d283663c5df44@ec2-34-193-123-212.compute-1.amazonaws.com:21179",
-    port: 21179,
-    password: "pd2c7acb80d6c99a31285b71a698ab86867df7eba0b55d3dd3c3d283663c5df44"
-});
+let client = undefined;
+if (process.env.NODE_ENV == "dev" || process.env.NODE_ENV == "test") {
+    client = redis_1.default.createClient();
+}
+else {
+    // client = redis.createClient({
+    //     host: "ec2-34-193-123-212.compute-1.amazonaws.com",
+    //     url: "redis://h:pd2c7acb80d6c99a31285b71a698ab86867df7eba0b55d3dd3c3d283663c5df44@ec2-34-193-123-212.compute-1.amazonaws.com:21179",
+    //     port: 21179,
+    //     password: "pd2c7acb80d6c99a31285b71a698ab86867df7eba0b55d3dd3c3d283663c5df44"
+    // });
+    client = redis_1.default.createClient({
+        host: process.env.REDIS_HOST,
+        url: process.env.REDIS_URL,
+        port: parseInt(process.env.REDIS_PORT),
+        password: process.env.PASSWORD
+    });
+}
 // client.on("connect", () => console.log("Connected to Redis"));
 client.on("error", (e) => console.log(`Unable to connect to Redis ${e.message}`));
 exports.default = client;
--- src/db/redis.ts
+++ src/db/redis.ts
@@ -8,12 +8,25 @@ import redis from "redis";
 /**
  * This function will use an URI to internet redis database.
  */
-const client = redis.createClient({
-    host: "ec2-34-193-123-212.compute-1.amazonaws.com",
-    url: "redis://h:pd2c7acb80d6c99a31285b71a698ab86867df7eba0b55d3dd3c3d283663c5df44@ec2-34-193-123-212.compute-1.amazonaws.com:21179",
-    port: 21179,
-    password: "pd2c7acb80d6c99a31285b71a698ab86867df7eba0b55d3dd3c3d283663c5df44"
-});
+let client: redis.RedisClient = undefined;
+
+if (process.env.NODE_ENV == "dev" || process.env.NODE_ENV == "test") {
+    client = redis.createClient();
+} else {
+    // client = redis.createClient({
+    //     host: "ec2-34-193-123-212.compute-1.amazonaws.com",
+    //     url: "redis://h:pd2c7acb80d6c99a31285b71a698ab86867df7eba0b55d3dd3c3d283663c5df44@ec2-34-193-123-212.compute-1.amazonaws.com:21179",
+    //     port: 21179,
+    //     password: "pd2c7acb80d6c99a31285b71a698ab86867df7eba0b55d3dd3c3d283663c5df44"
+    // });
+    client = redis.createClient({
+        host: process.env.REDIS_HOST,
+        url: process.env.REDIS_URL,
+        port: parseInt(process.env.REDIS_PORT),
+        password: process.env.PASSWORD
+    });
+}
+
 
 // client.on("connect", () => console.log("Connected to Redis"));
 client.on("error", (e) => console.log(`Unable to connect to Redis ${e.message}`));
